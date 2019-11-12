ca049423bcc597de0b8bb2282b90b3187fa54625
Simsoobin <55381009+Simsoobin@users.noreply.github.com>
2019-11-12 15:29:58 +0900

    11_2

--- main.c
+++ main.c
@@ -4,20 +4,15 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	char *pc;
-	int *pi;
-	double *pd;
 	
-	pc=(char*)10000;
-	pi=(int*)10000;
-	pd=(double*)10000;
+	int i =10;
+	int *ptr=&i;
 	
-	printf("증가전 : pc=%d, pi=%d, pd=%d\n", pc, pi, pd);
+	printf("i=%i, ptr=%p\n",  i, ptr);
+	(*ptr)++;
+	printf("i=%i, ptr=%p\n",  i, ptr);
+	*ptr++;
+	printf("i=%i, ptr=%p\n",  i, ptr);
 	
-	pc++;
-	pi++;
-	pd++;
-	printf("증가후: pc=%d, pi=%d, pd=%d\n", pc, pi, pd);
-
 	return 0;
 }
